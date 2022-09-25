52b97a820cc0b7e3273a30e6dfcf2c53d053ce40
Souptik Saha <iamthecubixnerd5@gmail.com>
2022-09-25 17:32:42 +0530

    Transitioned to PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import registerServiceWorker from 'react-service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
-registerServiceWorker.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
